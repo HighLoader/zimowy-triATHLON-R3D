205ad05d5e9d1f0381263632b2d49451e8467c76
HighLoader <109091530+HighLoader@users.noreply.github.com>
2023-01-29 10:59:32 +0000

    v4.1.2

---
+++ static/js/owl.carousel.js
@@ -0,0 +1,200 @@
+
+/*
+ *	jQuery OwlCarousel v1.31
+ *
+ *	Copyright (c) 2013 Bartosz Wojciechowski
+ *	http://www.owlgraphic.com/owlcarousel/
+ *
+ *	Licensed under MIT
+ *
+ */
+
+if ( typeof Object.create !== "function" ) {
+	Object.create = function( obj ) {
+		function F() {};
+		F.prototype = obj;
+		return new F();
+	};
+}
+(function( $, window, document, undefined ) {
+
+	var Carousel = {
+		init :function(options, el){
+			var base = this;
+
+			base.$elem = $(el);
+
+			// options passed via js override options passed via data attributes
+			base.options = $.extend({}, $.fn.owlCarousel.options, base.$elem.data(), options);
+
+			base.userOptions = options;
+			base.loadContent();
+		},
+
+		loadContent : function(){
+			var base = this;
+
+			if (typeof base.options.beforeInit === "function") {
+				base.options.beforeInit.apply(this,[base.$elem]);
+			}
+
+			if (typeof base.options.jsonPath === "string") {
+				var url = base.options.jsonPath;
+
+				function getData(data) {
+					if (typeof base.options.jsonSuccess === "function") {
+						base.options.jsonSuccess.apply(this,[data]);
+					} else {
+						var content = "";
+						for(var i in data["owl"]){
+							content += data["owl"][i]["item"];
+						}
+						base.$elem.html(content);
+					}
+					base.logIn();
+				}
+				$.getJSON(url,getData);
+			} else {
+				base.logIn();
+			}
+		},
+
+		logIn : function(action){
+			var base = this;
+
+			base.$elem.data("owl-originalStyles", base.$elem.attr("style"))
+					  .data("owl-originalClasses", base.$elem.attr("class"));
+
+			base.$elem.css({opacity: 0});
+			base.orignalItems = base.options.items;
+			base.checkBrowser();
+			base.wrapperWidth = 0;
+			base.checkVisible;
+			base.setVars();
+		},
+
+		setVars : function(){
+			var base = this;
+			if(base.$elem.children().length === 0){return false}
+			base.baseClass();
+			base.eventTypes();
+			base.$userItems = base.$elem.children();
+			base.itemsAmount = base.$userItems.length;
+			base.wrapItems();
+			base.$owlItems = base.$elem.find(".owl-item");
+			base.$owlWrapper = base.$elem.find(".owl-wrapper");
+			base.playDirection = "next";
+			base.prevItem = 0;
+			base.prevArr = [0];
+			base.currentItem = 0;
+			base.customEvents();
+			base.onStartup();
+		},
+
+		onStartup : function(){
+			var base = this;
+			base.updateItems();
+			base.calculateAll();
+			base.buildControls();
+			base.updateControls();
+			base.response();
+			base.moveEvents();
+			base.stopOnHover();
+			base.owlStatus();
+
+			if(base.options.transitionStyle !== false){
+				base.transitionTypes(base.options.transitionStyle);
+			}
+			if(base.options.autoPlay === true){
+				base.options.autoPlay = 5000;
+			}
+			base.play();
+
+			base.$elem.find(".owl-wrapper").css("display","block")
+
+			if(!base.$elem.is(":visible")){
+				base.watchVisibility();
+			} else {
+				base.$elem.css("opacity",1);
+			}
+			base.onstartup = false;
+			base.eachMoveUpdate();
+			if (typeof base.options.afterInit === "function") {
+				base.options.afterInit.apply(this,[base.$elem]);
+			}
+		},
+
+		eachMoveUpdate : function(){
+			var base = this;
+
+			if(base.options.lazyLoad === true){
+				base.lazyLoad();
+			}
+			if(base.options.autoHeight === true){
+				base.autoHeight();
+			}
+			base.onVisibleItems();
+
+			if (typeof base.options.afterAction === "function") {
+				base.options.afterAction.apply(this,[base.$elem]);
+			}
+		},
+
+		updateVars : function(){
+			var base = this;
+			if(typeof base.options.beforeUpdate === "function") {
+				base.options.beforeUpdate.apply(this,[base.$elem]);
+			}
+			base.watchVisibility();
+			base.updateItems();
+			base.calculateAll();
+			base.updatePosition();
+			base.updateControls();
+			base.eachMoveUpdate();
+			if(typeof base.options.afterUpdate === "function") {
+				base.options.afterUpdate.apply(this,[base.$elem]);
+			}
+		},
+
+		reload : function(elements){
+			var base = this;
+			setTimeout(function(){
+				base.updateVars();
+			},0)
+		},
+
+		watchVisibility : function(){
+			var base = this;
+
+			if(base.$elem.is(":visible") === false){
+				base.$elem.css({opacity: 0});
+				clearInterval(base.autoPlayInterval);
+				clearInterval(base.checkVisible);
+			} else {
+				return false;
+			}
+			base.checkVisible = setInterval(function(){
+				if (base.$elem.is(":visible")) {
+					base.reload();
+					base.$elem.animate({opacity: 1},200);
+					clearInterval(base.checkVisible);
+				}
+			}, 500);
+		},
+
+		wrapItems : function(){
+			var base = this;
+			base.$userItems.wrapAll("<div class=\"owl-wrapper\">").wrap("<div class=\"owl-item\"></div>");
+			base.$elem.find(".owl-wrapper").wrap("<div class=\"owl-wrapper-outer\">");
+			base.wrapperOuter = base.$elem.find(".owl-wrapper-outer");
+			base.$elem.css("display","block");
+		},
+
+		baseClass : function(){
+			var base = this;
+			var hasBaseClass = base.$elem.hasClass(base.options.baseClass);
+			var hasThemeClass = base.$elem.hasClass(base.options.theme);
+
+			if(!hasBaseClass){
+				base.$elem.addClass(base.options.baseClass);
+			}
